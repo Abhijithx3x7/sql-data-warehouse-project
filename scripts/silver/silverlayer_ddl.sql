9e91e604a1d5693ff5ff42f43cae3343d9080ddf
Abhijithx3x7 <85880103+Abhijithx3x7@users.noreply.github.com>
2025-03-02 11:52:39 -0500

    Update silverlayer_ddl.sql

--- scripts/silver/silverlayer_ddl.sql
+++ scripts/silver/silverlayer_ddl.sql
@@ -82,7 +82,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id              NVARCHAR(50),
     cat             NVARCHAR(50),
     subcat          NVARCHAR(50),
-    maintenance     NVARCHAR(50),
+    maintainance     NVARCHAR(50),
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
